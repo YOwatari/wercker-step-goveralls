5da9835d866b391438dc52dc6802e35258a88492
YOwatari <yuta_ohwatari@voyagegroup.com>
2016-04-17 03:04:01 +0900

    for multiple package

--- run.sh
+++ run.sh
@@ -11,9 +11,21 @@ fi
 go get github.com/axw/gocov/gocov
 go get github.com/mattn/goveralls
 
-go test --covermode=count -coverprofile=profile.cov ./...
+
+prof=${1:-".profile.cov"}
+echo "mode: count" > $prof
+gopath1=$(echo $GOPATH | cut -d: -f1)
+for pkg in $(go list ./...); do
+  tmpprof=$gopath1/src/$pkg/profile.tmp
+  go test -covermode=count -coverprofile=$tmpprof $pkg
+  if [ -f $tmpprof ]; then
+    cat $tmpprof | tail -n +2 >> $prof
+    rm $tmpprof
+  fi
+done
+
 
 GIT_BRANCH=$WERCKER_GIT_BRANCH
 goveralls -coverprofile=profile.cov -service=wercker.com -repotoken $COVERALLS_TOKEN
-                              
+
 
